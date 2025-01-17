d4a11c9ee31fda18c15eab225e03aefd3480d67b
Autobahntunnelfisch <cankinik@web.de>
2025-01-17 14:15:30 +0100

    create folder models and add class gallery_item

---
+++ lib/models/gallery_item.dart
@@ -0,0 +1,13 @@
+class GalleryItem {
+  final String imagePath;
+  final String imageTitle;
+  final String imageDate;
+  final String imageDescription;
+
+  GalleryItem({
+    required this.imagePath,
+    required this.imageTitle,
+    required this.imageDate,
+    required this.imageDescription,
+  });
+}
